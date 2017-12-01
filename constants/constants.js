cd378b42371e6ec78a891f70fdf04007aeb6884a
Cristian C. Zapata Torres (Cryzat) <cristianzapatat@gmail.com>
2017-11-30 19:00:53 -0500

    se añaden los estilos del mapa

--- constants/constants.js
+++ constants/constants.js
@@ -1,15 +1,15 @@
-let keyDistanceAndTime = 'AIzaSyCPDys-IZuq1CqhFr6cVEc-rMeT5Z33iKE'
-let keyGeocoding = 'AIzaSyCuixa1UeWyGrleFN_w7ceAJE9oYYmz1lU' // Google Maps Geocodign API
+let keyDistanceMatrix = 'AIzaSyACBAr9IbMFB22u2IVLlgdI6LHRJOI0P74'
+let keyGeocoding = 'AIzaSyC4PiNYLHiaDRYU63MQhb698_17WaSVCZI' // Google Maps Geocodign API
 
 module.exports = {
-  keyDistanceAndTime,
+  keyDistanceMatrix,
   keyGeocoding,
   getDistanceMatrix: (start, end) => {
     let startLoc = `${start.latitude},${start.longitude}`
     let endLoc = `${end.latitude},${end.longitude}`
     return `https://maps.googleapis.com/maps/api/distancematrix/json?
       units=imperial&origins=${startLoc}&destinations=${endLoc}&
-      key=${keyDistanceAndTime}&units=metric`
+      key=${keyDistanceMatrix}&units=metric`
   },
   getGeocoding: (pos) => {
     return `https://maps.google.com/maps/api/geocode/json?
