6c50cc419bbffe7576f44ea42e837e66f56880e5
Cristian Camilo Zapata Torres <cristianzapatat@gmail.com>
2017-12-22 22:28:01 -0500

    Ajuste de despliegue

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const app = require('./app')
 const config = require('./config')
 
-app.listen(config.port, () => {
-  console.log(`server run in the port: ${config.port}`)
-})
+app.listen(config.port)
