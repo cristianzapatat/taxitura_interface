a8e7ee4795ce70a36273d4426ddea1ea95fff7a0
Cristian C. Zapata Torres (Cryzat) <cristianzapatat@gmail.com>
2018-08-27 21:57:47 -0500

    Avance

--- socket/client.js
+++ socket/client.js
@@ -155,6 +155,38 @@ module.exports = (socket, socketClient, Service, db) => {
       err => console.log('Error acceptCancel', err))
   })
 
+  socket.on(_kts.socket.cancelServiceCab, (order, position) => {
+    console.log(order)
+    console.log(position)
+    Service.getId(order.service.id,
+      json => {
+        let orderAux = json.info
+        if (orderAux) {
+          orderAux.action = _kts.action.cancel
+          orderAux = Service.addTime(orderAux, _kts.json.cancelCab)
+          orderAux = Service.addTime(orderAux, _kts.json.cancel)
+          Service.update(orderAux,
+            _json => {
+              _fns.getClient(_json.info.cabman.id, socket).emit(_kts.socket.responseCancelServiceCab, _json.info)
+              if (position){
+                _fns.savePositionCab({
+                  id: _json.info.cabman.id,
+                  service: _json.info.service.id,
+                  action: _kts.json.cancelCab,
+                  position: position
+                }, db)
+              }
+            },
+            err => _fns.getClient(order.cabman.id, socket).emit(_kts.socket.responseCancelServiceCab, null)
+          )
+        } else {
+          _fns.getClient(order.cabman.id, socket).emit(_kts.socket.responseCancelServiceCab, null)
+        }
+      },
+      err => _fns.getClient(order.cabman.id, socket).emit(_kts.socket.responseCancelServiceCab, null)
+    )
+  })
+
   // Callback para almacenar la posición del taxista
   socket.on(_kts.socket.savePositionCab, data => {
     _fns.savePositionCab(data, db)
